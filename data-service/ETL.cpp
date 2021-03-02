6380003b15950b4fbf04e1723ee9b4f3c4e69a0f
27bslash <greenpear68@yahoo.co.uk>
2021-03-02 02:03:21 +0000

    added function to remove empty csv rows/cols

--- data-service/ETL.cpp
+++ data-service/ETL.cpp
@@ -6,38 +6,38 @@
 #include <string>
 #include <vector>
 #include <iterator>
-
+#include <regex>
 #include "sqlite3.h"
 
 #include "ETL_sql.h"
 
 #include <ctime>
-using std::time_t;
 using std::time;
+using std::time_t;
 
 using std::cout;
 using std::endl;
-using std::vector;
-using std::string;
 using std::map;
+using std::string;
+using std::vector;
 
 string file_to_string(const string &filename);
 vector<string> split_to_vec(const string &str, const char &delimiter);
-map<string, vector<string> > csv_to_map(const string &file_name);
-void db_holdings_input(sqlite3* db, map<string, vector<string> > raw_fund_data);
-int db_input(const string &in_file_name, sqlite3* db, const string &fund_format);
-sqlite3* open_db_connection(const string &db_file_name);
+void write_to_csv_file(string file_name, string output);
+map<string, vector<string>> csv_to_map(const string &file_name);
+void db_holdings_input(sqlite3 *db, map<string, vector<string>> raw_fund_data);
+int db_input(const string &in_file_name, sqlite3 *db, const string &fund_format);
+sqlite3 *open_db_connection(const string &db_file_name);
 
 string file_to_string(const string &filename)
 {
     std::ifstream in(filename);
 
     string result((std::istreambuf_iterator<char>(in)),
-                    std::istreambuf_iterator<char>());
+                  std::istreambuf_iterator<char>());
 
     return result;
 }
-
 
 vector<string> split_to_vec(const string &str, const char &delimiter)
 {
@@ -62,10 +62,9 @@ vector<string> split_to_vec(const string &str, const char &delimiter)
     return result;
 }
 
-
-map<string, vector<string> > csv_to_map(const string &file_name)
+map<string, vector<string>> csv_to_map(const string &file_name)
 {
-    map<string, vector<string> > csv_map;
+    map<string, vector<string>> csv_map;
 
     string file_string = file_to_string(file_name);
     vector<string> file_vec = split_to_vec(file_string, '\n');
@@ -73,16 +72,16 @@ map<string, vector<string> > csv_to_map(const string &file_name)
     // Initialise the map with pairs of header values and empty vectors.
     vector<string> headers = split_to_vec(file_vec[0], ',');
 
-    for(size_t i = 0; i != headers.size(); ++i)
-        csv_map.insert({ headers[i], vector<string>() });
+    for (size_t i = 0; i != headers.size(); ++i)
+        csv_map.insert({headers[i], vector<string>()});
 
-    // Fill the vectors associated with each header with the corresponding 
+    // Fill the vectors associated with each header with the corresponding
     // values, start iterating from 1 to avoid reading header into vectors.
-    for(size_t i = 1; i != file_vec.size(); ++i)
+    for (size_t i = 1; i != file_vec.size(); ++i)
     {
         vector<string> line_vec = split_to_vec(file_vec[i], ',');
 
-        for(size_t j = 0; j != headers.size(); ++j)
+        for (size_t j = 0; j != headers.size(); ++j)
         {
             csv_map[headers[j]].push_back(line_vec[j]);
         }
@@ -91,16 +90,15 @@ map<string, vector<string> > csv_to_map(const string &file_name)
     return csv_map;
 }
 
-
-void db_holdings_input(sqlite3* db, map<string, vector<string> > raw_fund_data)
+void db_holdings_input(sqlite3 *db, map<string, vector<string>> raw_fund_data)
 {
     string exchange = "NYSE";
     time_t t = time(0);
 
-    sqlite3_stmt* pStmt = nullptr;
+    sqlite3_stmt *pStmt = nullptr;
     sqlite3_prepare_v2(db, insert_into_holdings.c_str(), insert_into_holdings.size() + 1, &pStmt, nullptr);
 
-    for(size_t i = 1; i != raw_fund_data["fund"].size(); ++i)
+    for (size_t i = 1; i != raw_fund_data["fund"].size(); ++i)
     {
         sqlite3_bind_text(pStmt, 1, raw_fund_data["fund"][i].c_str(), raw_fund_data["fund"][i].size() + 1, NULL);
         sqlite3_bind_text(pStmt, 2, raw_fund_data["ticker"][i].c_str(), raw_fund_data["ticker"][i].size() + 1, NULL);
@@ -118,32 +116,78 @@ void db_holdings_input(sqlite3* db, map<string, vector<string> > raw_fund_data)
     sqlite3_finalize(pStmt);
 }
 
-
-int db_input(const string &in_file_name, sqlite3* db, const string &fund_format)
+int db_input(const string &in_file_name, sqlite3 *db, const string &fund_format)
 {
-    map<string, vector<string> > fund_data = csv_to_map(in_file_name);
+    map<string, vector<string>> fund_data = csv_to_map(in_file_name);
+    return 20;
 }
 
-
-sqlite3* open_db_connection(const string &db_file_name)
+sqlite3 *open_db_connection(const string &db_file_name)
 {
-    sqlite3* db;
+    sqlite3 *db;
 
     int conn = sqlite3_open(db_file_name.c_str(), &db);
 
-    if(conn)
+    if (conn)
         cout << "Error opening the database: " << sqlite3_errmsg(db) << endl;
     else
         cout << "Opened the database successfully." << endl;
-    
+
     return db;
 }
- 
+
+void remove_csv_footer(string fund_name)
+{
+    string file_string = file_to_string("ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv");
+    vector<string> lines = split_to_vec(file_string, '\n');
+    string result;
+    for (auto line : lines)
+    {
+        vector<string> line_split = split_to_vec(line, ',');
+        if (line_split.size() > 1)
+        {
+            for (size_t j = 0; j < line_split.size(); ++j)
+            {
+                string word = line_split[j];
+                if (word.size() > 0)
+                {
+                    result += word;
+                    if (j < line_split.size() - 1)
+                    {
+                        result += ",";
+                    }
+                    else
+                    {
+                        result += "\n";
+                    }
+                }
+            }
+        }
+    }
+    // this is retarded 
+    std::regex new_lines_re("\n$");
+    auto ouput = std::regex_replace(result, new_lines_re, "");
+    write_to_csv_file(fund_name, ouput);
+}
+
+void write_to_csv_file(string file_name, string output)
+{
+    string csv_name = file_name + ".csv";
+    std::ofstream output_file{csv_name};
+    if (!output_file)
+    {
+        std::cerr << "file open failed"
+                  << "\n";
+    }
+    output_file << output;
+    output_file.close();
+}
 
 int main()
 {
-    map<string, vector<string> > raw_fund_data = csv_to_map("ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv");
+    map<string, vector<string>> raw_fund_data = csv_to_map("ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv");
 
+    remove_csv_footer("arkk");
     // Pointer to SQLite connection
     sqlite3* db = open_db_connection("noah.db");
 
