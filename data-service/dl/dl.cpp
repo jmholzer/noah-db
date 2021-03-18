ae4c3fa2dcc143121b4df4eac9941329db28e9be
27bslash <greenpear68@yahoo.co.uk>
2021-03-18 20:10:01 +0000

    changed dl to use python wrapper

--- data-service/dl/dl.cpp
+++ data-service/dl/dl.cpp
@@ -18,53 +18,84 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
  *
- ***************************************************************************/ 
+ ***************************************************************************/
 
 #include "dl.h"
+// #include "../python/run_py.h"
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <python3.6m/Python.h>
+
+const char *get_scrape_data(const char *url)
+{
+    std::string file_path;
+    namespace fs = std::filesystem;
+    file_path = std::filesystem::current_path();
+    file_path += "/python";
+    setenv("PYTHONPATH", file_path.c_str(), 0);
+
+    Py_Initialize();
+
+    PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
+    // PyObject *sys = PyImport_ImportModule("sys");
+    // PyObject *path = PyObject_GetAttrString(sys, "path");
+    // PyList_Append(path, PyUnicode_FromString("."));
+
+    pName = PyUnicode_FromString("s");
+    if (pName == NULL)
+    {
+        // std::cout << "n";
+        PyErr_Print();
+    }
+    pModule = PyImport_Import(pName);
+    // Py_DECREF(pName);
+    if (pModule != NULL)
+    {
+        // std::cout << pModule;
+        pFunc = PyObject_GetAttrString(pModule, (char *)"download");
+        if (pFunc && PyCallable_Check(pFunc))
+        {
+            pArgs = PyTuple_Pack(1, PyUnicode_FromString((char *)url));
+            pValue = PyObject_CallObject(pFunc, pArgs);
+        }
+        auto result = _PyUnicode_AsString(pValue);
+        // std::cout << result << std::endl;
+        std::string s(result);
+        Py_Finalize();
+        return s.c_str();
+    }
+    else
+    {
+        PyErr_Print();
+        std::cout << "pModule is Null"
+                  << "\n";
+        EXIT_FAILURE;
+    }
+}
+std::string save_fund_data(std::string name, const char *url)
+{
+    std::time_t t = std::time(0);
+    std::tm *now = std::localtime(&t);
+    std::string file_name = "data/" + name + "_" + std::to_string(now->tm_year + 1900) + "_" + std::to_string(now->tm_mon + 1) + "_" + std::to_string(now->tm_mday) + ".csv";
+    std::ofstream myfile;
+    const char * data = get_scrape_data(url);
+    myfile.open(file_name);
+    myfile << std::string(data);
+    myfile.close();
+    return file_name;
+}
 
 std::vector<std::string> batch_dl(std::map<std::string, std::vector<std::string>> funds)
 {
     std::vector<std::string> file_names;
 
-    for(size_t i = 0; i != funds["name"].size(); ++i)
+    for (size_t i = 0; i != funds["name"].size(); ++i)
     {
         std::string file_name = save_fund_data(funds["name"][i], funds["url"][i].c_str());
         file_names.push_back(file_name);
     }
 
     return file_names;
-}
-
-std::string save_fund_data(std::string name, const char *url)
-{
-    std::time_t t = std::time(0);
-    std::tm* now = std::localtime(&t);
-    std::string file_name = "data/" + name
-                            + "_" + std::to_string(now->tm_year + 1900)
-                            + "_" + std::to_string(now->tm_mon + 1)
-                            + "_" + std::to_string(now->tm_mday)
-                            + ".csv";
-
-    FILE * out = fopen(file_name.c_str(), "w");
-
-
-    curl_global_init(CURL_GLOBAL_ALL);
-    CURL *curl;
-    curl = curl_easy_init();
-    CURLcode res;
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, out);
-
-    res = curl_easy_perform(curl);
-    if(res != CURLE_OK)
-    {
-        fprintf(stderr,"%s\n", curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
-        curl_global_cleanup();
-    }
-
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
-
-    return file_name;
 }
